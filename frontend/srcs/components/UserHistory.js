808e9706a65b4e28335d5709dbfdc82c0eccbc07
lyssoi <114637463+lyssoi@users.noreply.github.com>
2024-08-18 00:55:20 +0900

    feat:user matchhistory css setting

--- frontend/srcs/components/UserHistory.js
+++ frontend/srcs/components/UserHistory.js
@@ -8,22 +8,28 @@ export default function UserHistory ({btnnum}) {
         setHistory([
             {
                 opponent : 'wonyeong',
-                didwin : false
+                didwin : false,
+                day : '2024. 7. 21'
             }, {
                opponent : 'gaeul',
                didwin : true, 
+                day : '2023. 12. 21'
             }, {
                 opponent : 'eseo',
                 didwin : true,
+                day : '2023. 10. 21'
             }, {
                 opponent : 'yujin',
                 didwin : true,
+                day : '2023. 8. 12'
             }, {
                 opponent : 'liz',
                 didwin : false,
+                day : '2022. 2. 1'
             }, {
                 opponent : 'ray',
                 didwin : false,
+                day : '2021. 2. 6'
             }
         ]);
         setUser('haerin');
@@ -36,18 +42,23 @@ export default function UserHistory ({btnnum}) {
             ${
                 history.map((el)=>{
                    return `<div class="userFriend userHistory">
-                        <div>
-                            ${el?.didwin ? '🥇' : '&nbsp&nbsp&nbsp&nbsp'}
+                        <div class="matchday">
+                            ${el?.day}
                         </div>
-                        <div class="historyplayer historyplayerself ${el?.didwin ? `winner` : `loser`}">
-                            <p>${user}</p>
-                        </div>
-                        <div class="historyplayer historyplayeropponent ${el?.didwin ? `loser` : `winner`}">
-                            <p>${el?.opponent}</p>
-                        </div>
-                        <div>
-                            ${el?.didwin ? '&nbsp&nbsp&nbsp&nbsp' : '🥇'}
-                        </div>
+                        <div class="matchHistory">
+                             <div>
+                                 ${el?.didwin ? '🥇' : '&nbsp&nbsp&nbsp&nbsp'}
+                             </div>
+                             <div class="historyplayer historyplayerself ${el?.didwin ? `winner` : `loser`}">
+                                 <p>${user}</p>
+                             </div>
+                             <div class="historyplayer historyplayeropponent ${el?.didwin ? `loser` : `winner`}">
+                                 <p>${el?.opponent}</p>
+                             </div>
+                             <div>
+                                 ${el?.didwin ? '&nbsp&nbsp&nbsp&nbsp' : '🥇'}
+                             </div>
+                         </div>
                    </div>`
                 }).join('')
             }
