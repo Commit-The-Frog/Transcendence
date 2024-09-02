7dd2d36d0dbffea31459dba157e440d09a6e4f91
minjacho42 <louis9982.dev@gmail.com>
2024-09-02 11:38:32 +0900

    feat: Tournament Versus protocol modified

--- backend/remote_game/game/consumers.py
+++ backend/remote_game/game/consumers.py
@@ -2,13 +2,14 @@ import asyncio
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from .Game import Game
+from .Versus import Versus
 from .Tournament import Tournament
 from remote_game.game_objects.Player import Player
 import logging
 import urllib.parse
 
-game_dict = {}
-tournament_dict = {}
+versus_dict: dict[str, Versus] = {}
+tournament_dict: dict[str, Tournament] = {}
 
 logger = logging.getLogger('transcendence')
 
@@ -23,7 +24,7 @@ class GameConsumer(AsyncWebsocketConsumer):
         user_id = query_params.get('id', [None])[0]
         self.player = None
         if match_name and user_id:
-            self.match_group_name = f'game_{match_name}'
+            self.match_group_name = f'versus_{match_name}'
             self.player = Player(user_id)
             await self.channel_layer.group_add(
                 self.match_group_name, self.channel_name
@@ -33,16 +34,16 @@ class GameConsumer(AsyncWebsocketConsumer):
             await self.close()
             return
         logger.debug(f'{self.player.get_id()} connected to {self.match_group_name}')
-        if self.match_group_name not in game_dict: # 게임에 먼저 참가한다면,
-            logger.debug("GameDict Created")
-            game_dict[self.match_group_name] = Game(self.match_group_name)
-            self.game = game_dict[self.match_group_name]
-            self.game.add_player(self.player)
-            asyncio.create_task(self.game.start())
-        elif not game_dict[self.match_group_name].player_is_full(): # 이미 게임에 한명이 들어가있다면,
-            logger.debug("GameDict Joined")
-            self.game = game_dict[self.match_group_name]
-            self.game.add_player(self.player)
+        if self.match_group_name not in versus_dict: # 게임에 먼저 참가한다면,
+            logger.debug("Versus Created")
+            versus_dict[self.match_group_name] = Versus(self.match_group_name)
+            self.versus = versus_dict[self.match_group_name]
+            self.versus.add_player(self.player)
+            asyncio.create_task(self.versus.start())
+        elif not versus_dict[self.match_group_name].player_is_full(): # 이미 게임에 한명이 들어가있다면,
+            logger.debug("Versus Joined")
+            self.versus = versus_dict[self.match_group_name]
+            self.versus.add_player(self.player)
         else: # 게임에 이미 두명이 들어갔다면,
             await self.close(1000)
 
@@ -73,8 +74,8 @@ class GameConsumer(AsyncWebsocketConsumer):
     # Delete game from gamedict when game is done
     async def game_done(self, event):
         logger.debug(f'{self.match_group_name} game done')
-        if self.match_group_name in game_dict.keys():
-            game_dict.pop(self.match_group_name)
+        if self.match_group_name in versus_dict.keys():
+            versus_dict.pop(self.match_group_name)
 
 class TournamentConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -87,7 +88,7 @@ class TournamentConsumer(AsyncWebsocketConsumer):
         user_id = query_params.get('id', [None])[0]
         self.player = None
         if tournament_name and user_id:
-            self.tournament_group_name = f'game_{tournament_name}'
+            self.tournament_group_name = f'tournament_{tournament_name}'
             self.player = Player(user_id)
             await self.channel_layer.group_add(
                 self.tournament_group_name, self.channel_name
@@ -98,13 +99,13 @@ class TournamentConsumer(AsyncWebsocketConsumer):
             return
         logger.debug(f'TOURNAMENT: {self.player.get_id()} connected to {self.tournament_group_name}')
         if self.tournament_group_name not in tournament_dict.keys(): # 게임에 먼저 참가한다면,
-            logger.debug("TournamentDict Created")
+            logger.debug("Tournament Created")
             tournament_dict[self.tournament_group_name] = Tournament(self.tournament_group_name)
             self.tournament = tournament_dict[self.tournament_group_name]
             self.tournament.add_player(self.player)
             asyncio.create_task(self.tournament.start())
         elif not tournament_dict[self.tournament_group_name].player_is_full():
-            logger.debug("TournamentDict Joined")
+            logger.debug("Tournament Joined")
             self.tournament = tournament_dict[self.tournament_group_name]
             self.tournament.add_player(self.player)
         else:
@@ -134,7 +135,8 @@ class TournamentConsumer(AsyncWebsocketConsumer):
         event_json = json.dumps(event['data'])
         await self.send(text_data=event_json)
 
-    # Delete game from gamedict when game is done
+    # Delete game from tournament_dict when game is done
     async def game_done(self, event):
         logger.debug(f'{self.tournament_group_name} game done')
-        tournament_dict.pop(self.tournament_group_name)
+        if self.tournament_group_name in tournament_dict.keys():
+            tournament_dict.pop(self.tournament_group_name)
--- backend/remote_game/routing.py
+++ backend/remote_game/routing.py
@@ -5,6 +5,6 @@ from .waiting_queue import consumers as waiting_queue_consumers
 
 websocket_urlpatterns = [
     re_path(r"ws/game/lobby$", waiting_queue_consumers.WaitingQueueConsumer.as_asgi()),
-    re_path(r"ws/game/match$", game_consumers.GameConsumer.as_asgi()),
+    re_path(r"ws/game/1vs1$", game_consumers.GameConsumer.as_asgi()),
     re_path(r"ws/game/tournament$", game_consumers.TournamentConsumer.as_asgi()),
-]
+]1
