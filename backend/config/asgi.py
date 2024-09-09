b9a8e77ef628e76b8e95d47234d5e8728f1701d9
minjacho42 <louis9982.dev@gmail.com>
2024-09-09 15:02:35 +0900

    feat: asgi setting modified

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -8,25 +8,30 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.routing import ProtocolTypeRouter, URLRouter
 
+# Django 설정을 먼저 로드
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+# Django 애플리케이션 로드
+django_asgi_app = get_asgi_application()
+
+# 여기서 websocket_urlpatterns를 임포트하도록 함
 from remote_game.routing import websocket_urlpatterns
 
+# ASGI 애플리케이션 설정
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AllowedHostsOriginValidator(
             AuthMiddlewareStack(
                 # JWT 토큰 검증 미들웨어
                 # JWTAuthMiddleware,
                 URLRouter(websocket_urlpatterns),
             )
-        )
+        ),
     }
 )
