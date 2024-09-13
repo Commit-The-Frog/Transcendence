b25e93a55b8af76fe1ad437470bdd4dd9618aca0
dongyeuk <yupab3@naver.com>
2024-09-13 15:41:20 +0900

    fix: backend volume to apply modification

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -22,9 +22,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("api/game", include("remote_game.urls")),
-    path("api/admin", admin.site.urls),
-    path('api/login', include("login.urls")),
-    path('api/user', include("user.urls")),
-    path('api/match', include("match.urls")),
+    path("api/game/", include("remote_game.urls")),
+    path("api/admin/", admin.site.urls),
+    path('api/login/', include("login.urls")),
+    path('api/user/', include("user.urls")),
+    path('api/match/', include("match.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
