e6456a153edd4859e706506679afdb60b76e0762
dongyeuk <yupab3@naver.com>
2024-09-24 15:33:52 +0900

    feat: dev check2faView, insert 'win_rate' into return data

--- backend/login/urls.py
+++ backend/login/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     # path('2fa', include("login.urls")),
     path('callback', views.CallbackView.as_view()),
     path('2fa', views.SecondAuthView.as_view()),
-    path('logout', views.LogoutView.as_view()),
+    path('check2fa', views.Check2faView.as_view()),
+    path('logout/', views.LogoutView.as_view()),
     path('refresh', views.RefreshView.as_view()),
     path('islogin', views.LoginCheckView.as_view()),
     path('', views.ApiLoginView.as_view())
